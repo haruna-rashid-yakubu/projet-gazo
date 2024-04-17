186634b6fefb521a7dd930ad42254a0d0c86e88e
haruna-rashid-yakubu <haruna.rashid.yakubu@gmail.com>
2024-04-17 13:31:31 +0200

    config

---
+++ test.sh
@@ -0,0 +1,4 @@
+ sudo add-apt-repository ppa:openjdk-r/ppa 
+ sudo apt-get update 
+ sudo apt-get install openjdk-8-jdk 
+ sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
